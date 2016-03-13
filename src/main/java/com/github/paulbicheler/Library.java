2e804004e066bc069eb4453694e1232dcd19cb16
Paul Bicheler <paul.bicheler@gmail.com>
2016-03-13 08:06:09 +0100

    + Sample eclipse project with launch script, settings file, and some dummy classes. * Contains gradle wrapper 2.10

---
+++ src/main/java/com/github/paulbicheler/Library.java
@@ -0,0 +1,7 @@
+package com.github.paulbicheler;
+
+public class Library {
+	public boolean someLibraryMethod() {
+		return true;
+	}
+}
---
+++ src/main/java/com/github/paulbicheler/Main.java
@@ -0,0 +1,8 @@
+package com.github.paulbicheler;
+
+
+public class Main {
+    public void main(String args[]) {
+        // Stub main class
+    }
+}
